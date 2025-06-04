f3d25a55c529601aa9d781cf37d347cf4a58914d
shufi7 <johanvetran@gmail.com>
2025-06-04 11:42:36 +0800

    membuat css eksternal

--- komponen/script/diagnosis.js
+++ komponen/script/diagnosis.js
@@ -102,9 +102,9 @@ function tampilkanHasil() {
     }
 
     hasilDiagnosisHTML += `
-        <div class="d-flex justify-content-center justify-content-md-between flex-wrap mt-4 pt-3 border-top" style="border-color: #495057 !important;">
-            <button class="btn btn-secondary mb-2 mb-md-0 me-md-2" onclick="mulaiDiagnosis()"><i class="bi bi-arrow-clockwise"></i> Mulai Ulang Diagnosis</button>
-            <a href="pemesanan.html" class="btn btn-success"><i class="bi bi-cart-check"></i> Lanjut ke Pemesanan</a>
+          <div class="d-flex justify-content-center justify-content-md-between flex-wrap mt-4 pt-3 border-top" style="border-color: #495057 !important;">
+            <button class="btn btn-secondary mb-2 mb-md-0" onclick="mulaiDiagnosis()"><i class="bi bi-arrow-clockwise"></i> Mulai Ulang Diagnosis</button>
+            <a href="pemesanan.html" class="btn btn-success mb-2 mb-md-0"><i class="bi bi-cart-check"></i> Lanjut ke Pemesanan</a>
         </div>
     `;
     hasilDiv.innerHTML = hasilDiagnosisHTML;
